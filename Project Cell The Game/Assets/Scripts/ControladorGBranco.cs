ff0981f2737254242af6c47d6a461f064e2c9ffc
Rodrigo Azevedo Lima <rodrigoazlima@hotmail.com>
2016-04-21 22:42:32 -0300

    Adicionado movimentação base do globulo branco

---
+++ Project Cell The Game/Assets/Scripts/ControladorGBranco.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using Random = UnityEngine.Random;
+
+
+public class ControladorGBranco : MonoBehaviour
+{
+
+    // Controlador
+    public CharacterController cc;
+
+    // Atibutos
+    public int speed;
+    public int life;
+    public int attack;
+
+    //controle de desvio
+    private float desvioCursoAntigo = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateMoviment();
+    }
+
+
+    void updateMoviment()
+    {
+        var pos = cc.transform.position;
+        // Checar os limites do eixo Y para resetar
+        if ( pos.y > ToolsGlobal.limiteYSuperior
+            || pos.y < ToolsGlobal.limiteYInferior ) {
+
+            float rndX = Random.Range( ToolsGlobal.limiteXEsquerda, ToolsGlobal.limiteXDireita );
+            cc.transform.position = new Vector3( rndX,
+                ToolsGlobal.limiteYSuperior,
+                ToolsGlobal.ordemZ );
+
+        } else {// Se nao verifica bordas do eixo X
+
+            //velocidade
+            float speedTotal = ToolsGlobal.modifTemporal * - speed ;
+            //desvio
+            float desvio = Random.Range(-ToolsGlobal.modifDesvioCurso, ToolsGlobal.modifDesvioCurso );
+
+            // Desvio máximo/minimo
+            if ( desvio + desvioCursoAntigo > ToolsGlobal.modifDesvioMax ) {
+                desvio = ToolsGlobal.modifDesvioMax;
+            } else if ( desvio + desvioCursoAntigo < ToolsGlobal.modifDesvioMin ) {
+                desvio = ToolsGlobal.modifDesvioMin;
+            } else {
+                desvio += desvioCursoAntigo;
+            }
+
+            float x = speedTotal * desvio;
+            float y = speedTotal - x;
+            // Limite do eixo X
+            if ( ToolsGlobal.limiteXDireita < pos.x + x
+                || ToolsGlobal.limiteXEsquerda > pos.x + y + cc.transform.localScale.x ) {
+                desvio = -desvio;
+            }
+
+            desvioCursoAntigo = desvio;
+
+            x = speedTotal * desvio;
+            y = speedTotal - x;
+
+            print( "DesvioX: " + x + "\n" +
+                   "DesvioY: " + y + "\n" +
+                   "Pos:"+ cc.transform.position );
+            cc.Move(new Vector3(x, y, 0));
+        }
+    }
+}
---
+++ Project Cell The Game/Assets/Scripts/ToolsGlobal.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolsGlobal : MonoBehaviour {
+
+    // Modificadores temporal e de movimento
+    public static float modifTemporal = 0.004f;
+
+    //variaveis de movimento
+    public static Random rnd = new Random( );
+    public static float margemX = 1.2f;
+    public static float limiteYSuperior = 5.3f;
+    public static float limiteYInferior = -5.3f;
+    public static float limiteXEsquerda = -1.0f;
+    public static float limiteXDireita = 0.5f;
+    public static float ordemZ = -1.0f;
+    // variaveis de desvio de movimento
+    public static float modifDesvioCurso = 0.2f;
+    public static float modifDesvioMax = 0.4f;
+    public static float modifDesvioMin = -modifDesvioMax;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
